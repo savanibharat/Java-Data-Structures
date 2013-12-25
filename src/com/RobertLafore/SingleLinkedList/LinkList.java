e58d31c5835796aa84b1856511d43b9e603591fe
savanibharat <savanibharat@gmail.com>
2013-12-25 02:29:20 -0800

    delete(int key) and find(int key)

--- src/com/RobertLafore/SingleLinkedList/LinkList.java
+++ src/com/RobertLafore/SingleLinkedList/LinkList.java
@@ -1,47 +1,87 @@
 package com.RobertLafore.SingleLinkedList;
+
 //Refered book of Robert Lafore for Data Structures
 public class LinkList {
-	
+
 	private Link first;
-	
-	public LinkList(){
-		
-		first=null; 	//by default link is null but if you want to set null explicitly then this is how you do it
-		
+
+	public LinkList() {
+
+		first = null; // by default link is null but if you want to set null
+						// explicitly then this is how you do it
+
 	}
-	
-	public boolean isEmpty(){
-		
-		return (first==null);
-		
+
+	public boolean isEmpty() {
+
+		return (first == null);
+
 	}
-	
-	public void insertFirst(int iData,double dData){
-		
-		Link newLink=new Link(iData,dData);
-		newLink.next=first;
-		first=newLink;
-		
+
+	public void insertFirst(int iData, double dData) {
+
+		Link newLink = new Link(iData, dData);
+		newLink.next = first;
+		first = newLink;
+
 	}
-	
-	public Link deleteFirst(){
-		
-		Link temp=first;
-		first=first.next;
+
+	public Link deleteFirst() {
+
+		Link temp = first;
+		first = first.next;
 		return temp;
-		
+
 	}
-	public void displayList(){
-		
-		System.out.print("List (first-->last):");
-		Link current=first;
-		while(current!=null){
-			
-			current.display();
-			current=current.next;
+
+	public Link find(int key) {
+
+		Link current = first;
+		while (current.iData != key) {
+
+			if (current.next == null)
+				return null;
+			else
+				current = current.next;
 		}
-		
+		return current;
+
 	}
-	
-	
+
+	public Link delete(int key) {
+
+		Link current = first;
+		Link previous = first;
+		while (current.iData != key) {
+
+			if (current.next == null)
+				return null;
+			else {
+
+				previous = current;
+				current = current.next;
+
+			}
+		}
+		if (current == first)
+			first = first.next;
+		else
+			previous.next = current.next;
+
+		return current;
+
+	}
+
+	public void displayList() {
+
+		System.out.print("List (first-->last):");
+		Link current = first;
+		while (current != null) {
+
+			current.display();
+			current = current.next;
+		}
+
+	}
+
 }
--- src/com/RobertLafore/SingleLinkedList/LinkListApp.java
+++ src/com/RobertLafore/SingleLinkedList/LinkListApp.java
@@ -2,28 +2,37 @@ package com.RobertLafore.SingleLinkedList;
 
 public class LinkListApp {
 
-	public static void main(String args[]){
-		
-		LinkList theLinkList=new LinkList();
+	public static void main(String args[]) {
+
+		LinkList theLinkList = new LinkList();
 		theLinkList.insertFirst(11, 1.1);
 		theLinkList.insertFirst(22, 2.2);
 		theLinkList.insertFirst(33, 3.3);
 		theLinkList.insertFirst(44, 4.4);
-		
+
 		theLinkList.displayList();
-		System.out.println();
-		System.out.println("Deletion begins");
-		System.out.println();
-		while(!theLinkList.isEmpty()){
-			
-			Link aLink=theLinkList.deleteFirst();
-			System.out.print("Deleted");
-			aLink.display();
-			System.out.println(" ");
-			
-		}
-		
+
+		Link f = theLinkList.find(22);
+		if (f != null)
+			System.out.println("Found link");
+		else
+			System.out.println("Cant find");
+
+		f = theLinkList.delete(22);
+		if (f != null)
+			System.out.println("22 deleted");
+		else
+			System.out.println("Cannot find 22");
+		/*
+		 * System.out.println(); System.out.println("Deletion begins");
+		 * System.out.println(); while(!theLinkList.isEmpty()){
+		 * 
+		 * Link aLink=theLinkList.deleteFirst(); System.out.print("Deleted");
+		 * aLink.display(); System.out.println(" ");
+		 * 
+		 * }
+		 */
 		theLinkList.displayList();
-		
+
 	}
 }
