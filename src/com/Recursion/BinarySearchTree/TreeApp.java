df8191d19efde3ddece07a4355fd9c86495ef8ba
savanibharat <savanibharat@gmail.com>
2013-12-28 05:26:57 -0800

    Delete function

--- src/com/Recursion/BinarySearchTree/TreeApp.java
+++ src/com/Recursion/BinarySearchTree/TreeApp.java
@@ -3,21 +3,24 @@ package com.Recursion.BinarySearchTree;
 public class TreeApp {
 	public static void main(String[] args) {
 
-		int value;
 		Tree theTree = new Tree();
 		Node node;
 		theTree.insert(10);
 		theTree.insert(5);
-		theTree.insert(4);
-		theTree.insert(6);
+		theTree.insert(3);
+		theTree.insert(1);
+		theTree.insert(7);
 		theTree.insert(15);
 		theTree.insert(14);
 		theTree.insert(16);
 		theTree.traverse();
-		node=theTree.find(1);
-		if(node!=null)
+		node = theTree.find(1);
+		if (node != null)
 			System.out.println("found");
 		else
 			System.out.println("Not found");
-		}
+		theTree.delete(10);
+		theTree.traverse();
+		
+	}
 }
