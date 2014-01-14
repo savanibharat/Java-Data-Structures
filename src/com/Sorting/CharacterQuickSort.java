e04dc000998a1472692dadd7bdb65dd3e867b47b
savanibharat <savanibharat@gmail.com>
2014-01-13 18:19:10 -0800

    Recursive Quick Sort on characters

---
+++ src/com/Sorting/CharacterQuickSort.java
@@ -0,0 +1,71 @@
+package com.Sorting;
+
+public class CharacterQuickSort {
+
+	
+
+	private char[] numbers;
+	private int number;
+
+	public void sort(char[] values) {
+		// check for empty or null array
+		if (values == null || values.length == 0) {
+			return;
+		}
+		this.numbers = values;
+		number = values.length;
+		display();
+		quicksort(0, number - 1);
+	}
+
+	private void quicksort(int low, int high) {
+		int i = low, j = high;
+		// Get the pivot element from the middle of the list
+		int pivot = numbers[low + (high - low) / 2];
+
+		// Divide into two lists
+		while (i <= j) {
+			// If the current value from the left list is smaller then the pivot
+			// element then get the next element from the left list
+			while (numbers[i] < pivot) {
+				i++;
+			}
+			// If the current value from the right list is larger then the pivot
+			// element then get the next element from the right list
+			while (numbers[j] > pivot) {
+				j--;
+			}
+
+			// If we have found a values in the left list which is larger then
+			// the pivot element and if we have found a value in the right list
+			// which is smaller then the pivot element then we exchange the
+			// values.
+			// As we are done we can increase i and j
+			if (i <= j) {
+				exchange(i, j);
+				i++;
+				j--;
+			}
+		}
+		// Recursion
+		if (low < j)
+			quicksort(low, j);
+		if (i < high)
+			quicksort(i, high);
+	}
+
+	private void exchange(int i, int j) {
+		char temp = numbers[i];
+		numbers[i] = numbers[j];
+		numbers[j] = temp;
+	}
+
+	public void display() {
+
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.print(numbers[i] + " ");
+		}
+		System.out.println();
+
+	}
+}
---
+++ src/com/Sorting/CharacterQuickSortApp.java
@@ -0,0 +1,13 @@
+package com.Sorting;
+
+public class CharacterQuickSortApp {
+
+	public static void main(String[] args) {
+
+		char[] list = {'m','s','a','e','e','s','z'};
+		CharacterQuickSort q = new CharacterQuickSort();
+		q.sort(list);
+		q.display();
+
+	}
+}
