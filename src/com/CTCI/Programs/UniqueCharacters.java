cbe7cebba729fd40a152c688972192db06a20fc7
savanibharat <savanibharat@gmail.com>
2014-01-13 18:08:49 -0800

    Remove all Duplicate characters from String

---
+++ src/com/CTCI/Programs/UniqueCharacters.java
@@ -0,0 +1,30 @@
+package com.CTCI.Programs;
+
+public class UniqueCharacters {
+
+	public static void main(String[] args) {
+
+		String str = "aabbccdaadeeff";
+		boolean isUnique = isUniqueChar(str);
+		System.out.println(str + " " + isUnique);
+
+	}
+
+	public static boolean isUniqueChar(String str) {
+
+
+		boolean char_set[] = new boolean[256];
+		for (int i = 0; i < str.length(); i++) {
+
+			int val = str.charAt(i);
+			if (char_set[val]) {
+				return false;
+			} else {
+				char_set[val] = true;
+			}
+		}
+		return true;
+
+	}
+
+}
