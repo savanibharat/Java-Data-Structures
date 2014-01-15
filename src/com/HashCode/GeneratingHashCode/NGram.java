cdd8bf010ca1dcc154af1bcaf66677f89882d4c1
savanibharat <savanibharat@gmail.com>
2014-01-15 03:06:32 -0800

    NGram for Strings

--- src/com/HashCode/GeneratingHashCode/NGram.java
+++ src/com/HashCode/GeneratingHashCode/NGram.java
@@ -3,28 +3,63 @@ package com.HashCode.GeneratingHashCode;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Savani Bharat
+ *
+ * The Class NGram.
+ */
 public class NGram {
 
-    public static List<String> ngrams(int n, String str) {
-        List<String> ngrams = new ArrayList<String>();
-        String[] words = str.split(" ");
-        for (int i = 0; i < words.length - n + 1; i++)
-            ngrams.add(concat(words, i, i+n));
-        return ngrams;
-    }
+	/**
+	 * Ngrams.
+	 *
+	 * @param n the n
+	 * @param str the str
+	 * @return the list
+	 */
+	public static List<String> ngrams(int n, String str) {
+		
+		List<String> ngrams = new ArrayList<String>();
+		String[] words = str.split(" ");
+		
+		for (int i = 0; i < words.length - n + 1; i++)
+			ngrams.add(concat(words, i, i + n));
+		
+		return ngrams;
+	}
 
-    public static String concat(String[] words, int start, int end) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = start; i < end; i++)
-            sb.append((i > start ? " " : "") + words[i]);
-        return sb.toString();
-    }
+	/**
+	 * Concat.
+	 *
+	 * @param words the words
+	 * @param start the start
+	 * @param end the end
+	 * @return the string
+	 */
+	public static String concat(String[] words, int start, int end) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = start; i < end; i++)
+			sb.append((i > start ? " " : "") + words[i]);
+		
+		return sb.toString();
+	}
 
-    public static void main(String[] args) {
-        for (int n = 1; n <= 6; n++) {
-            for (String ngram : ngrams(n, "This is a program very good one wwith ngram working on it"))
-                System.out.println(ngram);
-            System.out.println();
-        }
-    }
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		
+		for (int n = 1; n <= 6; n++) {
+		
+			for (String ngram : ngrams(n,
+					"This is a program very good one wwith ngram working on it"))
+				System.out.println(ngram);
+			
+			System.out.println();
+		}
+	}
 }
