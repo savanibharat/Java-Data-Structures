70f4bc7c6373fbd406b7410871593fa9d7bc410a
savanibharat <savanibharat@gmail.com>
2014-01-31 22:34:28 -0800

    Homer's Rule for hashing
    Permutation of Input String leads us to collision.
    ex. pat,apt,tap

---
+++ src/com/HashCode/GeneratingHashCode/HonersRule.java
@@ -0,0 +1,43 @@
+package com.HashCode.GeneratingHashCode;
+
+import java.util.ArrayList;
+
+import com.ArrayList.PermutationOfStringRecursive.StringHelper;
+
+public class HonersRule {
+
+	public static void main(String[] args) {
+		
+		
+		ArrayList<String> manyStrings=StringHelper.PermutationFinder("bharat");//Total execution for abcdefghi 2531934499571
+		for (int i = 0; i < manyStrings.size(); i++) {
+			//hashing(manyStrings.get(i));
+		
+		}
+		hashing("abcd");
+		//hashing("bharat");
+
+		
+	}
+	public static void hashing(String s){
+		
+		int h=0;
+		char[] ch=s.toCharArray();
+		for (int i = s.length()-1; i >= 0; i--) {
+			h=(ch[i]+128*h)%127;
+			System.out.println(h);
+		}
+		System.out.println(h);
+		
+	}
+	
+}
+/*
+ * 100
+72
+43
+13
+13
+
+ * 
+ * */
