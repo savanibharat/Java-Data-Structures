35d54f6d1aa258213a5b36ec74afd1b01a4dde06
savanibharat <savanibharat@gmail.com>
2014-02-03 02:41:32 -0800

    Largest prime factor

---
+++ src/com/ArrayOperations/LargestPrimeFactor.java
@@ -0,0 +1,28 @@
+package com.ArrayOperations;
+
+public class LargestPrimeFactor{
+public static int largestPrimeFactor(long number) {
+    int i;
+    for (i = 3; i <= number; i++) {
+        if (number % i == 0) {
+            number /= i;
+            i--;
+        }
+    }
+    return i;
+}
+
+/*  change according to ur requirement. 
+public static long getLong(String prompt) {
+    System.out.print(prompt + " ");
+    long mrlong = input.nextLong();
+    input.nextLine();
+    return mrlong;
+}
+ */
+
+public static void main(String[] args) {
+    //long num = getLong("What number do you want to analyse? ");
+    System.out.println(largestPrimeFactor(365667778l));
+}
+}
