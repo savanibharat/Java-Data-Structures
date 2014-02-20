d9bc066e19b029294ef2b4ece4517e49455e2b94
savanibharat <savanibharat@gmail.com>
2014-02-20 02:23:30 -0800

    Remove particular character from String

---
+++ src/com/ArrayOperations/RemoveCharFromString.java
@@ -0,0 +1,20 @@
+package com.ArrayOperations;
+
+public class RemoveCharFromString {
+
+	public static void main(String[] args) {
+
+		String str = "abcadfaaderaagghfdr";
+		StringBuffer strBuffer = new StringBuffer();
+		char[] ch = str.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+
+			if (ch[i] == 'a' || ch[i] == 'A')
+				continue;
+			else
+				strBuffer.append(ch[i]);
+		}
+		System.out.println(strBuffer.toString());
+
+	}
+}
