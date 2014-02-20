bae760c3dd0af8edeea946bb718796c331827885
savanibharat <savanibharat@gmail.com>
2014-02-20 04:40:49 -0800

    Check whether number is power of 2 or not

---
+++ src/com/ArrayOperations/NumPower2.java
@@ -0,0 +1,21 @@
+package com.ArrayOperations;
+
+public class NumPower2 {
+
+	public static void main(String[] args) {
+		
+		int n=104;
+		
+		System.out.println(xy);
+		for(int i=1;i<=n/2;i++){
+			
+			double x=Math.pow(2,i);
+			if(x>n)
+				break;
+			else if(x==n)
+				System.out.println("2^"+i);
+		}
+		
+	}
+	
+}
