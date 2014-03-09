89b6e182646e27bec0d4b0644e3c1160b6a7f206
savanibharat <savanibharat@gmail.com>
2014-03-09 01:56:06 -0800

    Parenthesis checker

---
+++ src/com/ArrayOperations/AParenthesisChecker.java
@@ -0,0 +1,56 @@
+package com.ArrayOperations;
+
+import com.Stack.DelimiterMatching.StackX;
+
+public class AParenthesisChecker {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
+/*public void check() {
+
+		int stackSize = input.length();
+		StackX theStack = new StackX(stackSize);
+
+		for (int j = 0; j < input.length(); j++) {
+
+			char ch = input.charAt(j);
+			switch (ch) {
+
+			case '{':
+			case '[':
+			case '(':
+				theStack.push(ch);
+				break;
+			case '}':
+			case ']':
+			case ')':
+				if (!theStack.isEmpty()) {
+
+					char chx = theStack.pop();
+					if ((ch == '}' && chx != '{') || (ch == ']' && chx != '[') || (ch == ')' && chx != '(')) {
+
+						//System.out.println("nice one no err");
+						System.out.println("Error: "+ch+" at "+j);
+
+					}
+				} else
+					// prematurely empty[
+					System.out.println("Error: " + ch + " at " + j);
+				break;
+			default:
+				break;
+			}
+		}
+		// at this point all characters are processed
+		if (!theStack.isEmpty()) {
+
+			System.out.println("error: missing right delimiter");
+
+		}
+	}
+ * */
