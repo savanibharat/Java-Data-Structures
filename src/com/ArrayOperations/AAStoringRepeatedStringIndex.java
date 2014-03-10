4fa433718b8a239ae33e95ad9e4625e683a637dd
savanibharat <savanibharat@gmail.com>
2014-03-10 12:11:56 -0700

    storing repeated string index

--- src/com/ArrayOperations/AAStoringRepeatedStringIndex.java
+++ src/com/ArrayOperations/AAStoringRepeatedStringIndex.java
@@ -12,9 +12,11 @@ public class AAStoringRepeatedStringIndex {
 
 	public static void main(String[] args) throws Throwable {
 
+
 		Map<String, ArrayList<Integer>> hashMap = new ConcurrentHashMap<String, ArrayList<Integer>>();
 		ArrayList<Integer> c;
 
+		@SuppressWarnings("resource")
 		BufferedReader br = new BufferedReader(new FileReader(
 				"StringTokenizerInput.txt"));
 
@@ -23,17 +25,24 @@ public class AAStoringRepeatedStringIndex {
 
 			String[] newStr = str.toLowerCase().split(" ");
 			for (int i = 0; i < newStr.length; i++) {
+				if(newStr[i].length()>2){
 				c = new ArrayList<Integer>();
+
 				if (hashMap.containsKey(newStr[i])) {
 
 					c = hashMap.get(newStr[i]);
-					c.add(i);
-					hashMap.put(newStr[i], c);
-				} else {
+					if (!c.contains(i)) {
+						c.add(i);
+						hashMap.put(newStr[i], c);
+					}
+				}
+				else {
 
 					c.add(i);
 					hashMap.put(newStr[i], c);
+
 				}
+			}
 			}
 		}
 		System.out.println(hashMap);
@@ -41,6 +50,6 @@ public class AAStoringRepeatedStringIndex {
 }
 /*
  * 
-This is a String string my String
-{this=[0], a=[2], my=[5], string=[3, 4, 6], is=[1]}
+{well=[4], this=[0, 1], hope=[0], called=[5], string=[3, 4, 6], thing=[2], new=[3], savani=[7], works=[3], bharat=[6]}
+
  * */
