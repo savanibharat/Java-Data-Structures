fdca3807cf12ace85e7bc3488da13bf964e869ef
savanibharat <savanibharat@gmail.com>
2014-03-11 19:53:10 -0700

    HashChainApp

--- src/com/Hashing/SeparateChaining/HashChainApp.java
+++ src/com/Hashing/SeparateChaining/HashChainApp.java
@@ -1,41 +1,36 @@
 package com.Hashing.SeparateChaining;
 
-
 public class HashChainApp {
 
 	public static void main(String[] args) {
-		
-/*		HashTable theHashTable=new HashTable(10);
+
+		HashTable theHashTable = new HashTable(10);
 		int aKey;
 		Link theLink;
-		int a[]={1,6,5,4,9,13,16,17,23,53,7,177,166,133,2,4,7,98,43,59,47,6,5,29,4,65};
-		for(int j=0; j<a.length; j++) // insert data
-		{
-		//aKey = (int)(java.lang.Math.random() *size);
-			aKey=a[j];
-		System.out.println("akey is "+aKey);
+		int a[] = { 1, 6, 5, 4, 9, 13, 16, 17, 23, 53, 7, 177, 166, 133, 2, 4,
+				7, 98, 43, 59, 47, 6, 5, 29, 4, 65 };
+		for (int j = 0; j < a.length; j++) // insert data
+		{ 
+			aKey = (int) (java.lang.Math.random() * 10);
+			aKey = a[j];
+			System.out.println("akey is " + aKey);
 
-		theLink = new Link(aKey);
-		theHashTable.insert(theLink);
-		theHashTable.displayTable();
-		
+			theLink = new Link(aKey);
+			theHashTable.insert(theLink);
+			theHashTable.displayTable();
+
 		}
 		System.out.println();
 		System.out.println();
 		theHashTable.displayTable();
-		*/
-		SortedList s=new SortedList();
-		Link theLink;
-		int a[]={1,6,5,4,9,13,16,17,23,53,7,177,166,133,2,4,7,98,43,59,47,6,5,29,4,65};
-		for(int j=0; j<a.length; j++) // insert data
-		{
-		//aKey = (int)(java.lang.Math.random() *size);
-			int aKey=a[j];
-		System.out.println("akey is "+aKey);
 
-		theLink = new Link(aKey);
-		s.insert(theLink);
-	}
-		s.displayList();
-}
+		/*
+		 * SortedList s = new SortedList(); Link theLink; int a[] = { 1, 6, 5,
+		 * 4, 9, 13, 16, 17, 23, 53, 7, 177, 166, 133, 2, 4, 7, 98, 43, 59, 47,
+		 * 6, 5, 29, 4, 65 }; for (int j = 0; j < a.length; j++) // insert data
+		 * { // aKey = (int)(java.lang.Math.random() *size); int aKey = a[j];
+		 * System.out.println("akey is " + aKey);
+		 * 
+		 * theLink = new Link(aKey); s.insert(theLink); } s.displayList();
+		 */}
 }
