a3432a2181fbb90c8f98dab65a36bc283b4b9406
savanibharat <savanibharat@gmail.com>
2014-03-13 17:55:35 -0700

    reverse a sentence using split and StringBuilder

--- src/com/ArrayOperations/AReverseSentence.java
+++ src/com/ArrayOperations/AReverseSentence.java
@@ -1,37 +1,50 @@
 package com.ArrayOperations;
 
 public class AReverseSentence {
-	
+
 	public static void main(String[] args) {
-		
+
 		reverseSentence("THis sentence will be reversed");
 		rev("This will also be reversed");
 	}
+
 	public static void reverseSentence(String str) {
 		StringBuilder revStr = new StringBuilder("");
 		int end = str.length(); // substring takes the end index -1
 		int counter = str.length() - 1;
-		
+
 		for (int i = str.length() - 1; i >= 0; i--) {
-		
+
 			if (str.charAt(i) == ' ' || i == 0) {
-			
+
 				if (i != 0) {
-				
+
 					revStr.append(str.substring(i + 1, end));
 					revStr.append(" ");
-				} 
-				else {
-				
+				} else {
+
 					revStr.append(str.substring(i, end));
 				}
-				
+
 				end = counter;
 			}
-		
+
 			counter--;
 		}
 		System.out.println(revStr);
 	}
-	
+
+	public static void rev(String str) {
+
+		String[] newStr = str.split(" ");
+		StringBuilder string = new StringBuilder("");
+
+		for (int i = newStr.length - 1; i >= 0; i--) {
+
+			string.append(newStr[i]);
+			string.append(" ");
+		}
+		System.out.println(string);
+	}
+
 }
