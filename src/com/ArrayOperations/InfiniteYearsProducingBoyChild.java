7f688253b5c729d39c09a62e0e7259b5123a184f
savanibharat <savanibharat@gmail.com>
2014-03-19 23:10:54 -0700

    Boy:Girl ratio for infinite years

---
+++ src/com/ArrayOperations/InfiniteYearsProducingBoyChild.java
@@ -0,0 +1,28 @@
+package com.ArrayOperations;
+
+public class InfiniteYearsProducingBoyChild {
+
+}
+/***
+ * 
+ * 
+ * There is a village in which parent prefer to have at least 1 boy. 
+ * So they keep doing child until they get their first boy and then they stop doing children. 
+ * What is ratio of girl/boy in such town after infinite years.
+ * 
+ * After they all have their babies, the village will have 32 baby boys and 32 baby girls. The number of boys = number of girls. 
+ * Now, 32 families who had boys will stop having babies. 
+
+Of the remaining 32 families... 16 will have boys and 16 will have girls. The "total" number of baby boys = total number of baby girls = 32+16=48 
+
+Now, we have 16 families still going at it... of these, 8 boys and 8 girls will come out. Total number of boys = total number of girls = 32+16+8 = 56 
+
+Now, 8 couples will produce 4 boys and 4 girls. We have 60 boys and 60 girls total. 
+
+Next, 4 couples will have 2 boys and 2 girls. 62 boys and 62 girls 
+
+2 couples left... 63 boys and 63 girls 
+
+And finally we have only 1 couple left... Ignoring this pop couple who have had 8 babies in a row... As counter-intuitive as it looks, we can see that
+ the ratio of baby boys/baby girls remains constant.
+*/
