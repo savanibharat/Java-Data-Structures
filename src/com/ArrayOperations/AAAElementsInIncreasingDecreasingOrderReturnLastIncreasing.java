19cec0a91737eceaac347fcc3c4c7209451367bc
savanibharat <savanibharat@gmail.com>
2014-03-20 03:57:55 -0700

    input changed

--- src/com/ArrayOperations/AAAElementsInIncreasingDecreasingOrderReturnLastIncreasing.java
+++ src/com/ArrayOperations/AAAElementsInIncreasingDecreasingOrderReturnLastIncreasing.java
@@ -3,9 +3,9 @@ package com.ArrayOperations;
 public class AAAElementsInIncreasingDecreasingOrderReturnLastIncreasing {
 	public static int iter=0;
 		public static void main(String[] args){
-			int arr[] = {1,2,3,4,5,6,7,8,9,0};
+			int arr[] = {1,2,3,4,5,6,7,6,2,1};
 			System.out.println(searchLargest(arr,0,arr.length));
-			System.out.println(iter);
+			//System.out.println(iter);
 		}
 		
 		public static int searchLargest(int[] array,int start,int end){
@@ -13,6 +13,9 @@ public class AAAElementsInIncreasingDecreasingOrderReturnLastIncreasing {
 			while ( start <= end){
 				
 				int mid = start + (end -start)/2;
+				System.out.println("start is "+start);
+				System.out.println("end is "+end);
+				System.out.println(mid);
 				if ( mid == array.length-1)return -1;
 				if(  array[mid] > array[mid-1] && array[mid] > array[mid+1]){
 					iter++;
