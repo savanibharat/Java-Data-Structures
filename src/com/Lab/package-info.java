454deff5b45ac064a0b1f074f48f757c681a1d5f
savanibharat <savanibharat@gmail.com>
2014-04-01 02:26:51 -0700

    created package lab

---
+++ src/com/Lab/package-info.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+/**
+ * @author Savani Bharat
+ *
+ */
+package com.Lab;
+/*hashmap
+anagram
+ threading, locking, deadlock avoidance, priority inversion 
+ tree traversal and optimization
+ strings and depth-first search
+multithread questions
+Have you done performance testing? Have you done stress testing? 
+Have you done load testing? How would you set up a test plan 
+(out of nothing)?  
+ finding the nth element from the end and reversing the linked list
+shortest path in a binary search tree 
+ special punctuation
+merge sorted arrays
+Binary search trees, finding odd number counts, hashmap vs hashtable, 
+JDK nuances
+how do you test a function that adds two numbers
+ Write a program to determine if a character array is an anagram of another character array
+What is a static function? What is a static variable?  
+What is a virtual destructor and why would you use it?
+calculator take to calculate addition of two 32 bit numbers*/
