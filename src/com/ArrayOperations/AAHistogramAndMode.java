759a059f0ba72ed64b549f5c042e2fdbde5f9d55
savanibharat <savanibharat@gmail.com>
2014-04-04 15:38:01 -0700

    Histogram and mode

--- src/com/ArrayOperations/AAHistogramAndMode.java
+++ src/com/ArrayOperations/AAHistogramAndMode.java
@@ -1,5 +1,8 @@
 package com.ArrayOperations;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AAHistogramAndMode {
 
 	public static void main(String[] args) {
@@ -11,7 +14,14 @@ public class AAHistogramAndMode {
 			b[a[i]] = b[a[i]] + (++counter);
 			counter = 0;
 		}
-		int max = 1;
+		
+		for (int i = 0; i < b.length; i++) {
+			//if(b[i]==1)
+			//System.out.println(i+"  "+b[i]);
+		}
+		int g=my(a);	
+		System.out.println(g);
+		/*int max = 1;
 		int index = 0;
 		for (int i = 0; i < b.length; i++) {
 			if (b[i] > 0) {
@@ -29,7 +39,57 @@ public class AAHistogramAndMode {
 
 			}
 		}
-
+*/
+	}
+	public static int my(int [] aa){
+		
+		/*if(a[0]==a[1])
+			return a[0];
+		int b[] = new int[a.length+1];
+		int counter = 0;
+		for (int i = 0; i < a.length; i++) {
+			b[a[i]] = b[a[i]] + (++counter);
+			counter = 0;
+		}
+  		int i=0;
+  		for (i = 0; i < b.length; i++) {
+			
+  			
+  			if(b[i]==1){
+				return i;
+        	}
+		}
+  		return -1;*/
+		
+		int a[]={0,0,1,2,1};
+		
+  		HashMap<Integer, Integer> hashMap=new HashMap<Integer,Integer>();
+  		if(a.length==1)
+  			return a[0];
+  		
+    	if(a.length==2)
+            if(a[0]==a[1])
+  			return a[0];
+    	
+    	for (int i = 0; i < a.length; i++) {
+			
+    		if(hashMap.containsKey(a[i]))
+    			hashMap.put(a[i],hashMap.get(a[i])+1);
+    		else
+    			hashMap.put(a[i],1);
+    		
+		}
+    	
+  		for (Map.Entry<Integer,Integer> entry : hashMap.entrySet()) {
+  		  Integer key=entry.getKey();
+  		  Integer value = entry.getValue();
+  		  
+  		  if(value==1)
+  			  return key;
+  		  
+  		}
+		return -1;
+  		
 	}
 }
 /*
