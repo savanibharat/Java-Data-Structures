42c2d2b8009fa74e186948048a14da2046e51370
savanibharat <savanibharat@gmail.com>
2014-04-06 23:33:48 -0700

    acsii

--- src/com/ArrayOperations/AAAWordHistogram.java
+++ src/com/ArrayOperations/AAAWordHistogram.java
@@ -1,12 +1,13 @@
 package com.ArrayOperations;
 
-
+//# and _
 public class AAAWordHistogram {
 
 	public static void main(String[] args) {
 
 		String str="abc,.';?><:s12dh#jdhf874354r';[]r-rew./.,ds/f';'l'`1`3p@#&^@%^%#";
-		String s1[]=str.split("[\\[\\]\\!$%&'()*+,-./:;<=>?@\\^_`{|}~΅Ά£¤¥¦§¨©ª«¬­®―°±²³΄µ¶·ΈΉΊ»Ό½ΎΏΐΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡÒΣΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώÿ?]+");
+		String s1[]=str.split("[\\[\\]\\!$%&'()*+,-./:;<=>?@\\^`{|}~΅Ά£¤¥¦§¨©ª«¬­®―°±²³΄µ¶"
+				+ "·ΈΉΊ»Ό½ΎΏΐΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡÒΣΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώÿ?]+");
 		for (int i = 0; i < s1.length; i++) {
 			System.out.println(i+" "+s1[i]);
 		}
