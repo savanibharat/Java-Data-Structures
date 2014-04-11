c5e3474ef48f0f3531e632284450d5030941daf4
savanibharat <savanibharat@gmail.com>
2014-04-10 20:18:34 -0700

    Node.java commit

--- src/com/BinarySearchTree/Node.java
+++ src/com/BinarySearchTree/Node.java
@@ -1,15 +1,25 @@
-package com.Recursion.BinarySearchTree;
+package com.BinarySearchTree;
 
+/**
+ * @author Savani Bharat
+ *
+ */
 public class Node {
 
 	public int iData;
 	public Node rightChild;
 	public Node leftChild;
-	
-	public void displayNode(){
-		
-		System.out.println(" { "+iData+" } ");
-		
+public Node() {
+
+}
+	public void displayNode() {
+
+		System.out.println(" { " + iData + " } ");
+
 	}
-	
+
+	public Node(int a) {
+		iData = a;
+	}
+
 }
